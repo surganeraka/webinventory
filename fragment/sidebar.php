77cfc19bb6a2a87404ba3325bd46fe7017e7d46b
surganeraka <108063045+surganeraka@users.noreply.github.com>
2022-06-24 20:11:02 +0700

    Update sidebar
    - Update Daftar Barang Keluar dan Tambah Barang Keluar

--- fragment/sidebar.php
+++ fragment/sidebar.php
@@ -132,7 +132,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="index.php?hal=tambah_barang_keluar" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Tambah Barang Keluar</p>
                     </a>
